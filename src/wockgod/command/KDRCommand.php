55623a6469af20007c58d03d30d5bc0c9a30c86e
Shreyansh <shreyanshop20+2@outlook.com>
2023-05-27 19:44:32 +0530

    UI Implementation

--- src/wockgod/command/KDRCommand.php
+++ src/wockgod/command/KDRCommand.php
@@ -6,6 +6,8 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\player\Player;
 use wockgod\KDR;
+use jojoe77777\FormAPI\SimpleForm;
+use jojoe77777\FormAPI\CustomForm;
 
 class KDRCommand extends Command {
 
@@ -19,14 +21,28 @@ class KDRCommand extends Command {
             $sender->sendMessage("§cThis command can only be used in-game.");
             return;
         }
+        
         $playerName = $sender->getName();
         $kills = KDR::getInstance()->getConfig()->getNested("players.$playerName.kills", 0);
         $deaths = KDR::getInstance()->getConfig()->getNested("players.$playerName.deaths", 0);
         $kdr = $deaths > 0 ? round($kills / $deaths, 2) : $kills;
-        $message = KDR::getInstance()->getConfig()->getNested("messages.kdr", "§aYour kill/death ratio is {kdr} (Kills: {kills}, Deaths: {deaths}).");
-        $message = str_replace("{kdr}", $kdr, $message);
-        $message = str_replace("{kills}", $kills, $message);
-        $message = str_replace("{deaths}", $deaths, $message);
-        $sender->sendMessage($message);
+
+        $form = new SimpleForm(function (Player $player, ?int $data) use ($kills, $deaths) {
+            if ($data !== null) {
+                if ($data === 0) {
+                    // Action when the "Confirmed" button is clicked
+                    $player->sendMessage("Your KDR has been confirmed.");
+                    // Add any additional actions here
+                }
+            }
+        });
+
+        $form->setTitle("KDR Statistics");
+        $form->setContent("Kill/Death Ratio: $kdr\nKills: $kills\nDeaths: $deaths");
+
+        // Add the "Confirmed" button
+        $form->addButton("Confirmed");
+
+        $sender->sendForm($form);
     }
 }
