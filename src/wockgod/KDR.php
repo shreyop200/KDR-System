d98cccf435ca07bad7fa4e83ffdd2efc4804e82d
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-05-26 18:00:15 -0700

    Update KDR.php
    Removed onDisable

--- src/wockgod/KDR.php
+++ src/wockgod/KDR.php
@@ -9,33 +9,23 @@ use wockgod\command\KDRCommand;
 class KDR extends PluginBase {
 
     private static $instance;
+
     /** @var Config */
     private $config;
+
     public function onEnable(): void
     {
         self::$instance = $this;
         $this->registerCommands();
         $this->saveDefaultConfig();
         $this->getServer()->getPluginManager()->registerEvents(new KDRListener(), $this);
-
         $this->config = $this->getConfig();
-        foreach ($this->getServer()->getOnlinePlayers() as $player) {
-            $playerName = $player->getName();
-            if (!$this->config->exists("players.$playerName")) {
-                $this->config->setNested("players.$playerName.kills", 0);
-                $this->config->setNested("players.$playerName.deaths", 0);
-            }
-        }
+
     }
 
     public static function getInstance(): self
     {
         return self::$instance;
-    }
-
-    public function onDisable(): void
-    {
-        $this->config->save();
     }
 
     public function registerCommands(){
