722b671afe0221141ef7b7c61b6c9c36da90339e
iLVOEWOCK <131495628+iLVOEWOCK@users.noreply.github.com>
2023-05-26 17:37:40 -0700

    Add files via upload

---
+++ src/wockgod/KDRListener.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace wockgod;
+
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\player\Player;
+
+class KDRListener implements Listener {
+
+
+    public function onPlayerDeath(PlayerDeathEvent $event) {
+        $player = $event->getPlayer();
+        $cause = $player->getLastDamageCause();
+        if ($cause instanceof EntityDamageByEntityEvent) {
+            $damager = $cause->getDamager();
+            if ($damager instanceof Player) {
+                $damagerName = $damager->getName();
+                $entityName = $player->getName();
+                $damagerKills = KDR::getInstance()->getConfig()->getNested("players.$damagerName.kills", 0);
+                $entityDeaths = KDR::getInstance()->getConfig()->getNested("players.$entityName.deaths", 0);
+                KDR::getInstance()->getConfig()->setNested("players.$damagerName.kills", $damagerKills + 1);
+            }
+        }
+        $entityName = $player->getName();
+        $entityDeaths = KDR::getInstance()->getConfig()->getNested("players.$entityName.deaths", 0);
+        KDR::getInstance()->getConfig()->setNested("players.$entityName.deaths", $entityDeaths + 1);
+    }
+}
